ba0c6683492046ad84205129da1d2526053e698d
RKNITH <raviranjankofficial@gmail.com>
2024-06-20 15:19:28 +0530

    first commit

---
+++ Backend/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+import userRoute from "./routes/user.route.js";
+import messageRoute from "./routes/message.route.js";
+import { app, server } from "./SocketIO/server.js";
+
+dotenv.config();
+
+// middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+const PORT = process.env.PORT || 3001;
+const URI = process.env.MONGODB_URI;
+console.log('MONGODB_URI:', process.env.MONGODB_URI);
+
+// MongoDB connection
+mongoose.connect(URI)
+    .then(() => console.log("Mongodb connected"))
+    .catch((error) => {
+        console.error("MongoDB connection error:", error);
+        process.exit(1); // Exit the process if unable to connect to MongoDB
+    });
+
+// routes
+app.use("/api/user", userRoute);
+app.use("/api/message", messageRoute);
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
